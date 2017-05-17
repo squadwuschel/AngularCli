084c0efa7fb688ddc7ebd52a2b9b3df480bb664d
SquadWuschel <joecorporation@gmx.de>
2017-05-17 21:00:18 +0200

    erster Commit

---
+++ AngularCliTest/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ButtonsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
